0887eb7db6cb377d4f214f1f664aa6e0cfdd2936
Railag <whenthegroundcavedin@gmail.com>
2018-03-05 18:45:07 +0300

    fixes

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -5,11 +5,13 @@ import com.intel.bluetooth.RemoteDeviceHelper;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ProgressIndicator;
 import javafx.scene.shape.Circle;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
@@ -35,8 +37,8 @@ public class Main extends Application {
 
     private final static int PACKAGE_SIZE = 5;
 
-    private final static int WIDTH = 1920;
-    private final static int HEIGHT = 1080;
+    private static int width = 1920; // default width
+    private static int height = 1080; // default height
 
     private final static double THRESHOLD_ACCELEROMETER_MAX = 7.0;
 
@@ -60,26 +62,41 @@ public class Main extends Application {
     private Stage primaryStage;
     private ProgressIndicator progress;
     private Button bluetoothButton;
+    private DiscoveryAgent agent;
+
+    private WaitThread waitThread;
+    private ProcessConnectionThread processConnectionThread;
+    private int transactionSearchId;
 
     @Override
     public void start(Stage primaryStage) {
         this.primaryStage = primaryStage;
-        //    Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        //    primaryStage.setTitle("Hello World");
 
+        Rectangle2D primaryScreenBounds = Screen.getPrimary().getVisualBounds();
+
+        width = (int) primaryScreenBounds.getWidth();
+        height = (int) primaryScreenBounds.getHeight();
+
+        //set Stage boundaries to visible bounds of the main screen
+        primaryStage.setX(primaryScreenBounds.getMinX());
+        primaryStage.setY(primaryScreenBounds.getMinY());
+        primaryStage.setWidth(width);
+        primaryStage.setHeight(height);
 
         // init circle
         circle = new Circle();
         circle.setCenterX(0.0f);
         circle.setCenterY(0.0f);
+        circle.setLayoutX(width / 2);
+        circle.setLayoutY(height / 2);
         circle.setRadius(15.0f);
         circle.setVisible(false);
 
         // init loading indicator
         progress = new ProgressIndicator();
         progress.setPrefSize(100, 100);
-        progress.setLayoutX(WIDTH / 2);
-        progress.setLayoutY(HEIGHT / 2);
+        progress.setLayoutX(width / 2);
+        progress.setLayoutY(height / 2);
         progress.setVisible(false);
 
         // init bluetooth discover button
@@ -90,12 +107,12 @@ public class Main extends Application {
                 bluetoothInit();
             }
         });
-        bluetoothButton.setLayoutX(WIDTH / 2);
-        bluetoothButton.setLayoutY(HEIGHT / 2);
+        bluetoothButton.setLayoutX(width / 2);
+        bluetoothButton.setLayoutY(height / 2 - 50);
 
         Group root = new Group(circle, progress, bluetoothButton);
 
-        primaryStage.setScene(new Scene(root, WIDTH, HEIGHT));
+        primaryStage.setScene(new Scene(root, width, height));
         primaryStage.show();
 
         login();
@@ -135,6 +152,7 @@ public class Main extends Application {
     }
 
     public static final Object lock = new Object();
+    private boolean discovered;
     private DiscoveryListener discoveryListener = new DiscoveryListener() {
 
         @Override
@@ -162,12 +180,18 @@ public class Main extends Application {
         public void serviceSearchCompleted(int arg0, int arg1) {
             System.out.println("serviceSearchCompleted");
 
+            if (!discovered) {
+                discover();
+            }
+
             //        broadcastCommand("Hello world!");
         }
 
         @Override
         public void servicesDiscovered(int arg0, ServiceRecord[] arg1) {
             System.out.println("servicesDiscovered");
+
+            discovered = true;
 
             serviceRecords.addAll(Arrays.asList(arg1));
 
@@ -180,6 +204,28 @@ public class Main extends Application {
         }
     };
 
+    private void discover() {
+        if (agent == null) {
+            System.out.println("Discover: null agent");
+            return;
+        }
+
+        try {
+            for (RemoteDevice device : remoteDevices) {
+                String name = device.getFriendlyName(false);
+                if (name.equalsIgnoreCase("Railag")) {
+                    UUID[] uuidSet = new UUID[1];
+                    uuidSet[0] = new UUID(0x0100);
+                    UUID[] uuids = new UUID[1];
+                    uuids[0] = new UUID("0cbb85aa795141a6b891b2ee53960860", false);
+                    transactionSearchId = agent.searchServices(null, uuids, device, discoveryListener);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     private void establishConnection() {
         for (ServiceRecord record : serviceRecords) {
             String url = record.getConnectionURL(ServiceRecord.NOAUTHENTICATE_NOENCRYPT, false);
@@ -191,27 +237,9 @@ public class Main extends Application {
                     RemoteDeviceHelper.authenticate(record.getHostDevice());
 
                     stopLoading();
-                    ProcessConnectionThread processConnectionThread = new ProcessConnectionThread(connection);
+                    processConnectionThread = new ProcessConnectionThread(connection);
                     processConnectionThread.start();
-                    /*DataInputStream dataInputStream = connection.openDataInputStream();
 
-                    ArrayList<Double> x = new ArrayList<Double>();
-
-                    while (true) { // waiting for data
-                        try {
-                            for (int i = 0; i < 10; i++) {
-                                x.add(dataInputStream.readDouble());
-                            }
-
-                            break;
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    }
-
-                    for (Double d : x) {
-                        System.out.println(d);
-                    }*/
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
@@ -244,7 +272,7 @@ public class Main extends Application {
                 LocalDevice localDevice = LocalDevice.getLocalDevice();
 
                 // 2
-                DiscoveryAgent agent = localDevice.getDiscoveryAgent();
+                agent = localDevice.getDiscoveryAgent();
 
                 // 3
                 agent.startInquiry(DiscoveryAgent.GIAC, discoveryListener);
@@ -261,65 +289,19 @@ public class Main extends Application {
                /* UUID[] uuids = new UUID[1];
                 uuids[0] = new UUID(0x1101);*/
 
-                for (RemoteDevice device : remoteDevices) {
-                    String name = device.getFriendlyName(false);
-                    if (name.equalsIgnoreCase("Railag")) {
-                        UUID[] uuidSet = new UUID[1];
-                        uuidSet[0] = new UUID(0x0100);
-                        int[] attrIds = {0x0100};
-                        //       agent.searchServices(attrIds, uuidSet, device, discoveryListener);
-                        UUID[] uuids = new UUID[1];
-                        uuids[0] = new UUID("0cbb85aa795141a6b891b2ee53960860", false);
-                        agent.searchServices(null, uuids, device, discoveryListener);
-
-                       /* String bt_addr = device.getBluetoothAddress(); //remDev is the remote device; I am sure it's the correct one
-                        try {
-                            StreamConnection connection = (StreamConnection) Connector.open("btspp://" + bt_addr + ":1;authenticate=false;encrypt=false;master=true;");
-                            System.out.println("connected");
-                            InputStream inputStream = connection.openInputStream();
-                            System.out.println("inputStream");
-                        } catch (javax.bluetooth.BluetoothConnectionException e) {
-                            e.printStackTrace();
-                        }*/
-                    }
-                }
+                discover();
 
             } catch (Exception e) {
                 e.printStackTrace();
             }
+        }
 
-            /*LocalDevice local;
-
-            StreamConnectionNotifier notifier;
-            StreamConnection connection;
-
-            // setup the server to listen for connection
-            try {
-                local = LocalDevice.getLocalDevice();
-                local.setDiscoverable(DiscoveryAgent.GIAC);
-
-
-
-                UUID uuid = new UUID(80087355); // "04c6093b-0000-1000-8000-00805f9b34fb"
-                String url = "btspp://localhost:" + uuid.toString() + ";name=RemoteBluetooth";
-                notifier = (StreamConnectionNotifier) Connector.open(url);
-            } catch (Exception e) {
-                e.printStackTrace();
-                return;
+        public void cancel() {
+            if (agent != null) {
+                agent.cancelInquiry(discoveryListener);
+                agent.cancelServiceSearch(transactionSearchId);
+                transactionSearchId = -1;
             }
-            // waiting for connection
-            while(true) {
-                try {
-                    System.out.println("waiting for connection...");
-                    connection = notifier.acceptAndOpen();
-
-                    Thread processThread = new Thread(new ProcessConnectionThread(connection));
-                    processThread.start();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    return;
-                }
-            }*/
         }
     }
 
@@ -329,8 +311,6 @@ public class Main extends Application {
 
         // Constant that indicate command from devices
         private static final int EXIT_CMD = -1000;
-        private static final int KEY_RIGHT = 1;
-        private static final int KEY_LEFT = 2;
 
         public ProcessConnectionThread(StreamConnection connection) {
             mConnection = connection;
@@ -340,14 +320,11 @@ public class Main extends Application {
         public void run() {
             try {
                 // prepare to receive data
-                //    InputStream inputStream = mConnection.openInputStream();
-
                 System.out.println("waiting for input");
 
                 DataInputStream dataInputStream = mConnection.openDataInputStream();
                 while (true) {
 
-                    //    int command = inputStream.read();
                     double value = dataInputStream.readDouble();
                     //        System.out.println("Received: " + value);
 
@@ -374,113 +351,43 @@ public class Main extends Application {
                         break;
                     }
 
-                    processCommand(value);
                 }
             } catch (EOFException eofException) {
                 // socket closed on server side
-
+            //    eofException.printStackTrace();
+                stopBluetooth();
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }
 
-        /**
-         * Process the command from client
-         *
-         * @param command the command code
-         */
-        private void processCommand(double command) {
-            try {
-               /* Robot robot = new Robot();
-                switch (command) {
-                    case KEY_RIGHT:
-                        robot.keyPress(KeyEvent.VK_RIGHT);
-                        System.out.println("Right");
-                        break;
-                    case KEY_LEFT:
-                        robot.keyPress(KeyEvent.VK_LEFT);
-                        System.out.println("Left");
-                        break;
-                }*/
-            } catch (Exception e) {
-                e.printStackTrace();
+        public void cancel() {
+            if (mConnection != null) {
+                try {
+                    mConnection.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         }
     }
 
     private void moveCircle() {
-        //    for (int i = 0; i < xCurrent.size() && i < yCurrent.size(); i++) {
-        double x = adjust(xCurrent.get(xCurrent.size() - 1), WIDTH, true);
-        double y = adjust(yCurrent.get(yCurrent.size() - 1), HEIGHT, false);
+        for (int i = 0; i < xCurrent.size() && i < yCurrent.size(); i++) {
+            double x = adjust(xCurrent.get(xCurrent.size() - 1), width, true);
+            double y = adjust(yCurrent.get(yCurrent.size() - 1), height, false);
 
-        circle.setLayoutX(x);
-        circle.setLayoutY(y);
-        System.out.println("X: " + x + " Y: " + y);
-        //     }
-
-     /*   try {
-            Thread.sleep(50);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }*/
+            circle.setLayoutX(x);
+            circle.setLayoutY(y);
+            System.out.println("X: " + x + " Y: " + y);
+        }
     }
 
     private void bluetoothInit() {
         startLoading();
 
-        WaitThread thread = new WaitThread();
-        thread.start();
-
-       /* RConnectorService restService = restService();
-        Call<AccelerometerResult> call = restService.fetchAccelerometerData(userId); // 292
-        call.enqueue(new Callback<AccelerometerResult>() {
-            @Override
-            public void onResponse(Call<AccelerometerResult> call, Response<AccelerometerResult> response) {
-                AccelerometerResult result = response.body();
-                if (result == null) {
-                    //            onError(new IllegalArgumentException());
-                    return;
-                }
-                if (result.invalid()) {
-                    //            toast(result.error);
-                    return;
-                }
-
-                List<AccelerometerResult.AccelerometerResults> resultsList = result.accelerometerResults;
-                for (AccelerometerResult.AccelerometerResults r : resultsList) {
-                    int eventId = r.id;
-                    if (eventId > latestEventId) {
-                        latestEventId = eventId;
-                    } else if (latestEventId == eventId) {
-                        continue;
-                    }
-
-                    List<Double> xArray = r.x;
-                    List<Double> yArray = r.y;
-                    for (int i = 0; i < xArray.size() && i < yArray.size(); i++) {
-                        double x = adjust(xArray.get(i), WIDTH, true);
-                        double y = adjust(yArray.get(i), HEIGHT, false);
-
-                        try {
-                            Thread.sleep(100);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-
-                        circle.setLayoutX(x);
-                        circle.setLayoutY(y);
-                        System.out.println("X: " + x + " Y: " + y);
-                    }
-                }
-
-                bluetoothInit();
-            }
-
-            @Override
-            public void onFailure(Call<AccelerometerResult> call, Throwable throwable) {
-                throwable.printStackTrace();
-            }
-        });*/
+        waitThread = new WaitThread();
+        waitThread.start();
     }
 
     private float adjust(double paramToAdjust, double maxValue, boolean inverse) { // 7 - xResolutionMax (e.g. 1920), 3 - y?
@@ -527,5 +434,19 @@ public class Main extends Application {
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    private void stopBluetooth() {
+        if (waitThread != null) {
+            waitThread.cancel();
+        }
+
+        if (processConnectionThread != null) {
+            processConnectionThread.cancel();
+        }
+
+        discovered = false;
+
+        circle.setVisible(false);
     }
 }
